8c279b1d54c858c57ec0c4987f0bd577f5869d88
maziyargh83 <mazyar.gh83@gmail.com>
2023-05-31 10:59:36 +0330

    fix build err

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const GAME = new game(canvas);
+new game(canvas);
